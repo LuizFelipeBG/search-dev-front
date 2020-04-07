b842433fca1b0d35c3de9a9fdebc4020339fa038
Luiz Felipe Barbosa <luiz.barbosa@xdevel.com.br>
2020-04-07 15:25:58 -0300

    feat: button header styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './header.css'
 
 const style = {
     headerContainer:{ 
         display: 'flex', 
-        justifyContent: 'center', 
+        justifyContent: 'flex-end', 
         alignItems: 'center',
-        padding: '7px'
+        padding: '7px',
     }
 
 }
@@ -13,7 +14,7 @@ const Header = React.memo(({ shouldBeInRegisterPage, setshouldBeInRegisterPage }
 
     return (
         <div style={style.headerContainer}>
-            <button onClick={() => setshouldBeInRegisterPage(currentValue => !currentValue)}>{shouldBeInRegisterPage ? 'FILTRAR POR TEC' : 'REGISTRAR DEV'}</button>
+            <button className="button-header"  onClick={() => setshouldBeInRegisterPage(currentValue => !currentValue)}>{shouldBeInRegisterPage ? 'PROCURAR POR TEC' : 'REGISTRAR DEV'}</button>
         </div>
     );
 })
